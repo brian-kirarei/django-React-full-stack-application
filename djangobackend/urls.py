ccd9417c54fe3632e1ea8f1ba440308891857e70
BRIAN KIRAREI <briankirarei393@gmail.com>
2025-05-13 12:50:36 +0300

    first commit

---
+++ djangobackend/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.http import JsonResponse
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('', lambda request: JsonResponse({'message': 'Welcome to the Django-React API'})),
+]
